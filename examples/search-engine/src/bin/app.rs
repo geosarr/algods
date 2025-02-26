abb4b2fa4ed93e5940af3cf1cb33efd8acf06282
Georges SARR <georgesmbissanes@gmail.com>
2025-02-26 20:44:34 +0100

    Improved query result formatting.

--- examples/search-engine/src/bin/app.rs
+++ examples/search-engine/src/bin/app.rs
@@ -1,9 +1,18 @@
 use rocket::{get, launch, routes};
+use search::{loader::Loader, model::Boolean};
 mod engine;
 
 #[get("/boolean/search/<query>")]
 async fn boolean_search(query: String) -> String {
-    query
+    let loader = Loader::from("enwiki-latest-abstract.xml.gz".to_string()).unwrap();
+    let model = Boolean::new();
+    let (index, collection) = loader.load(500);
+    model
+        .retrieve(&query, &index, &collection)
+        .iter()
+        .map(|doc| format!("{}\n\n", doc.content()))
+        .take(100)
+        .collect()
 }
 
 #[launch]
